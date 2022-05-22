b9a45fa553f0d4f160758f9a3d8996c75f568767
eunjin <jklkm90@gmail.com>
2022-05-22 14:38:45 +0900

    request() 공통 기능 분리

--- src/main/java/kr/co/skb/agent/util/CommunicationUtil.java
+++ src/main/java/kr/co/skb/agent/util/CommunicationUtil.java
@@ -1,80 +1,69 @@
 package kr.co.skb.agent.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import kr.co.skb.agent.device.KickboardLocation;
-import kr.co.skb.agent.device.KickboardUse;
+import kr.co.skb.agent.domain.RequestInfo;
+import kr.co.skb.agent.domain.KickboardLocation;
+import kr.co.skb.agent.domain.KickboardUse;
+import kr.co.skb.agent.domain.Kickboard;
 import lombok.extern.log4j.Log4j2;
 import okhttp3.*;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.Map;
 
 @Log4j2
 @Component
 public class CommunicationUtil {
-    @Value("#{${ip}}")
-    private Map<String, String> ip;
-    @Value("#{${url}}")
-    private Map<String, String> url;
+    @Autowired RequestInfo.Ip ip;
+    @Autowired RequestInfo.Url url;
 
     public boolean request(final Kickboard kickboard) throws IOException, JSONException {
-        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
-        OkHttpClient client = new OkHttpClient();
-        ObjectMapper objectMapper = new ObjectMapper();
-        String json = objectMapper.writeValueAsString(kickboard);
-        RequestBody body = RequestBody.create(JSON, json);
-
-        Request request = new Request.Builder()
-                .url("http://" + ip.get("system") + url.get("info"))
-                .post(body)
-                .build();
-
-        Response response = client.newCall(request).execute();
-        ResponseBody responseBody = response.body();
-        JSONObject code = new JSONObject(responseBody.string());
+        String requestUrl = "http://" + ip.getSystem() + url.getInfo();
+        JSONObject code = new JSONObject(request(kickboard, requestUrl).string());
 
         return "200".equals(code.getString("code"));
     }
 
     public boolean request(final KickboardUse kickboardUse) throws IOException, JSONException {
-        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
-        OkHttpClient client = new OkHttpClient();
-        ObjectMapper objectMapper = new ObjectMapper();
-        String json = objectMapper.writeValueAsString(kickboardUse);
-        RequestBody body = RequestBody.create(JSON, json);
-
-        Request request = new Request.Builder()
-                .url("http://" + ip.get("helmet") + url.get("use"))
-                .post(body)
-                .build();
-
-        Response response = client.newCall(request).execute();
-        ResponseBody responseBody = response.body();
-        JSONObject code = new JSONObject(responseBody.string());
+        String requestUrl = "http://" + ip.getHelmet() + url.getUse();
+        JSONObject code = new JSONObject(request(kickboardUse, requestUrl).string());
 
         return "200".equals(code.getString("code"));
     }
 
     public boolean request(final KickboardLocation kickboardLocation) throws IOException, JSONException {
+        String requestUrl = "http://" + ip.getSystem() + url.getLocation();
+        JSONObject code = new JSONObject(request(kickboardLocation, requestUrl).string());
+
+        return "200".equals(code.getString("code"));
+    }
+
+    /*
+    공통 기능 분리
+    대상: request()
+        : 킥보드 정보 송신, 킥보드 사용 정보 송신, 킥보드 위치 정보 송신
+     */
+    public ResponseBody request(Object obj, String requestUrl) throws IOException, JSONException {
         MediaType JSON = MediaType.parse("application/json; charset=utf-8");
         OkHttpClient client = new OkHttpClient();
         ObjectMapper objectMapper = new ObjectMapper();
-        String json = objectMapper.writeValueAsString(kickboardLocation);
+        String json = objectMapper.writeValueAsString(obj);
         RequestBody body = RequestBody.create(JSON, json);
 
         Request request = new Request.Builder()
-                .url("http://" + ip.get("system") + url.get("location"))
+                .url(requestUrl)
                 .post(body)
                 .build();
 
         Response response = client.newCall(request).execute();
         ResponseBody responseBody = response.body();
-        JSONObject code = new JSONObject(responseBody.string());
 
-        return "200".equals(code.getString("code"));
+        log.info(request.body().toString());
+        log.info(responseBody.string());
+
+        return responseBody;
     }
 }
