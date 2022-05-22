1c5d6ac2979293f89082085684d6721dd7518f19
eunjin <jklkm90@gmail.com>
2022-05-22 14:43:40 +0900

    킥보드 정보 확인 로직 변경, null 체크 메소드 이동

--- src/main/java/kr/co/skb/agent/util/KickboardUtil.java
+++ src/main/java/kr/co/skb/agent/util/KickboardUtil.java
@@ -1,39 +1,18 @@
 package kr.co.skb.agent.util;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.co.skb.agent.domain.Kickboard;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Log4j2
 @Component
 public class KickboardUtil {
-    @Value("${kickboard}")
-    private String kickboard;
+    @Autowired private Kickboard kickboard;
 
-    public Kickboard getKickboard() throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
+    public Kickboard getKickboard() {
+        log.info(this.kickboard);
 
-        Kickboard kickboard = objectMapper.readValue(
-                this.kickboard, new TypeReference<Kickboard>() {});
-        log.info(kickboard);
-
-        return kickboard;
-    }
-
-    public boolean isEmpty(final Kickboard kickboard) {
-        if (kickboard == null) {
-            return true;
-        } else if (kickboard.getIp().trim().length() < 1
-                || kickboard.getModel().trim().length() < 1
-                || kickboard.getIp().trim().length() < 1) {
-            return true;
-        }
-
-        return false;
+        return this.kickboard;
     }
 }
